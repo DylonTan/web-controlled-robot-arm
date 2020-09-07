fd64eaa07132ebba49c2887d60f3dffb59a01499
DylonTan <dylontan.2003@gmail.com>
2020-09-08 00:53:56 +0800

    Update arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,27 +1,29 @@
 #include <Servo.h>
 #define SERVO_PIN_1 6 // Base
-#define SERVO_PIN_2 9
+#define SERVO_PIN_2 9 // Limb
 #define SERVO_PIN_3 10
 #define SERVO_PIN_4 11
 
 Servo servo1;
 Servo servo2;
-int angle = 0;
+int xAxisAngle = 0;
+int zAxisAngle = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  
   servo1.attach(SERVO_PIN_1);
   servo2.attach(SERVO_PIN_2);
+  Serial.println("Arduino started.");
 }
 
 String data;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while (Serial.available()) {
+  while (Serial.available() > 0) {
     data = Serial.readString();
+   
     if (data == "FORWARDS") {
       forwardsHandler();
     } else if (data == "LEFT") {
@@ -36,32 +38,26 @@ void loop() {
 
 void forwardsHandler() {
   Serial.println("MOVING MOTOR FORWARDS");
-  if (angle + 20 <= 180) {
-    angle = angle + 20;
-    servo2.write(angle);
-  }
+  servo2.write(90);
 }
 
 void leftHandler() {
   Serial.println("MOVING MOTOR TO THE LEFT");
-  if (angle - 20 <= 180) {
-    angle = angle - 20;
-    servo1.write(angle);
+  if (xAxisAngle - 20 >= 0) {
+    xAxisAngle = xAxisAngle - 20;
+    servo1.write(xAxisAngle);
   }
 }
 
 void rightHandler() {
   Serial.println("MOVING MOTOR TO THE RIGHT");
-  if (angle + 20 <= 180) {
-    angle = angle + 20;
-    servo1.write(angle);
+  if (xAxisAngle + 20 <= 180) {
+    xAxisAngle = xAxisAngle + 20;
+    servo1.write(xAxisAngle);
   }
 }
 
 void backwardsHandler() {
   Serial.println("MOVING MOTOR BACKWARDS");
-  if (angle - 20 <= 180) {
-    angle = angle - 20;  
-    servo2.write(angle);
-  }
+  servo2.write(0);
 }
